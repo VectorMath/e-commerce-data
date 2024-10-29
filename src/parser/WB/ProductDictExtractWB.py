3b108afc8d6d102beaa2ebc3ed3357b6eb26578a
VectorMath <ev.zharov1998@gmail.com>
2024-10-30 00:51:08 +0200

    upd: Rename class ParserDictWB on ProductDictExtractWB and make him static

--- src/parser/WB/ProductDictExtractWB.py
+++ src/parser/WB/ProductDictExtractWB.py
@@ -4,23 +4,24 @@ from src.parser.WB import constants
 from src import config
 
 
-class ParserDictWB:
-    """Class that make work of extracting data from dictionary.
-    Using in ParserWB.
+class ProductDictExtractWB:
+    """Static class that make work of extracting data from dictionary.
+    Using in method parse_product of class ParserWB.
     """
 
-    def __init__(self, data: dict):
-        """Class constructor
-        :param data: the dictionary that need to be extract
+    def __new__(cls, *args, **kwargs):
+        """In case if someone want to create object of this class
         """
-        self._data: dict = data
+        raise TypeError("Unable to create static class")
 
-    def get_table_size(self) -> str:
+    @staticmethod
+    def get_table_size(data: dict) -> str:
         """ Method that get table size value
+        :param data: response dictionary
         :return: string of table size
         """
         sizes: list[str] = []
-        sizes_table = self._data.get(constants.PRODUCT_SIZES_TABLE, config.NULL_VALUE)
+        sizes_table = data.get(constants.PRODUCT_SIZES_TABLE, config.NULL_VALUE)
 
         if sizes_table is not config.NULL_VALUE:
             values = sizes_table.get(constants.PRODUCT_DETAIL_KEY_VALUES, config.NULL_VALUE)
@@ -46,12 +47,14 @@ class ParserDictWB:
         sizes = sorted(sizes, key=extract_min_value)
         return constants.SPLIT_VALUE.join(sizes)
 
-    def get_characteristic_from_options(self, name_type: Any) -> str:
+    @staticmethod
+    def get_characteristic_from_options(data: dict, name_type: Any) -> str:
         """Method that extract characteristic values from options-key
+        :param data: response dictionary
         :param name_type: type of characteristic
         :return:  value in string format
         """
-        characteristics: list[dict] = self._data.get(constants.PRODUCT_DETAIL_LIST_KEY, config.NULL_VALUE)
+        characteristics: list[dict] = data.get(constants.PRODUCT_DETAIL_LIST_KEY, config.NULL_VALUE)
 
         if characteristics is not config.NULL_VALUE:
             for item in characteristics:
