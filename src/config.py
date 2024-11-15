c5f00b31e1b013b43f1c9929dcfc8ddf35ed12fb
VectorMath <ev.zharov1998@gmail.com>
2024-11-15 21:34:13 +0200

    Change note:
    - Create dag that create table with actual prices of products;
    
    - upd: Add in src.config.py constants with table names of price and price-history in db, change host from localhost to postgres, rename env-var from DATABASE_NAME to POSTGRES_DB;
    
    - upd: Add postgres variables in x-airflow-common;

--- src/config.py
+++ src/config.py
@@ -9,5 +9,8 @@ NULL_VALUE = None
 
 POSTGRES_USERNAME = os.environ.get('POSTGRES_USERNAME')
 POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-POSTGRES_DB_NAME = os.environ.get('DATABASE_NAME')
-POSTGRES_HOST = "localhost"
+POSTGRES_DB_NAME = os.environ.get('POSTGRES_DB')
+POSTGRES_HOST = "postgres"
+
+PRICE_HISTORY_TABLE = "price_history"
+PRICE_TABLE = "price"
