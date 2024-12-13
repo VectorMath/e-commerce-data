f83fcd3dd944ab43e2d6c083c03a89c5f2869c89
VectorMath <ev.zharov1998@gmail.com>
2024-12-14 00:10:02 +0200

    upd: remove constants that's not using and add data type

--- src/parser/WB/constants.py
+++ src/parser/WB/constants.py
@@ -1,116 +1,110 @@
-# Count value after which need make sleep in ParserWB.parse_product_list
-PRODUCT_LIST_PAGE_NUMBER_FOR_SLEEP = 5
-
 # Duration of sleep by seconds, use in lib "time".
-TIME_FOR_SLEEP = 2
+TIME_FOR_SLEEP: int = 2
 
 # Index of every element in list after that need make sleep
-COUNTER_OF_INDEX_FOR_SLEEP_IN_LOOP = 10
+COUNTER_OF_INDEX_FOR_SLEEP_IN_LOOP: int = 10
 
 # Count value after which need make sleep in AsyncRequesterWB._find_api_url_from_network
-ASYNC_REQUESTER_SLEEP_INDEX_VALUE = 5
+ASYNC_REQUESTER_SLEEP_INDEX_VALUE: int = 5
 
-CHUNK_SIZE = 25
+CHUNK_SIZE: int = 25
 
 # Duration of timeout by millisecond after you go to page in AsyncRequesterWB._find_api_url_from_network
-ASYNC_REQUESTER_TIMEOUT = 1000
+ASYNC_REQUESTER_TIMEOUT: int = 1000
 
-SYMBOL_TO_REPLACE_FOR_PAGE_NUMBER_IN_URL = '*'
+SYMBOL_TO_REPLACE_FOR_PAGE_NUMBER_IN_URL: str = '*'
 
-SPLIT_VALUE = ', '
+SPLIT_VALUE: str = ', '
 
 # JSON object, that contain data about personal info of product
-REQUIRED_JSON_FOR_PRODUCT_PERSONAL_INFO = "card.json"
+REQUIRED_JSON_FOR_PRODUCT_PERSONAL_INFO: str = "card.json"
 
 # Part of URL that need to change for getting URL for price history
-REQUIRED_JSON_FILE_FOR_CHANGE = "ru/card.json"
+REQUIRED_JSON_FILE_FOR_CHANGE: str = "ru/card.json"
 
 # JSON object, that contain data about price history of product
-REQUIRED_JSON_FOR_PRODUCT_PRICE_HISTORY = "price-history.json"
+REQUIRED_JSON_FOR_PRODUCT_PRICE_HISTORY: str = "price-history.json"
 
-DATA_KEY = 'data'
-PRODUCTS_KEY = 'products'
-ID_KEY = 'id'
-ROOT_KEY = 'root'
-NAME_KEY = 'name'
+DATA_KEY: str = 'data'
+PRODUCTS_KEY: str = 'products'
+ID_KEY: str = 'id'
+ROOT_KEY: str = 'root'
+NAME_KEY: str = 'name'
 
-PRODUCT_ID = 'product_id'
-ROOT_ID = 'root_id'
-PRODUCT_NAME = 'product_name'
-DATE = 'date'
+PRODUCT_ID: str = 'product_id'
+ROOT_ID: str = 'root_id'
+PRODUCT_NAME: str = 'product_name'
+DATE: str = 'date'
 
-PRICE_HISTORY_DATE_KEY = "dt"
-PRICE_HISTORY_PRICE_KEY = "price"
-PRICE_HISTORY_CURRENT_CURRENCY_KEY = "RUB"
+PRICE_HISTORY_DATE_KEY: str = "dt"
+PRICE_HISTORY_PRICE_KEY: str = "price"
+PRICE_HISTORY_CURRENT_CURRENCY_KEY: str = "RUB"
 
-FIRST_PAGE = 1
-LAST_PAGE = 3
+FIRST_PAGE: int = 1
+LAST_PAGE: int = 3
 
-PRODUCT_ROOT_ID = "imt_id"
-PRODUCT_NAME_KEY = "imt_name"
-PRODUCT_CATEGORY = "subj_root_name"
-PRODUCT_MAIN_CATEGORY = "subj_name"
-PRODUCT_DESCRIPTION = "description"
+PRODUCT_ROOT_ID: str = "imt_id"
+PRODUCT_NAME_KEY: str = "imt_name"
+PRODUCT_CATEGORY: str = "subj_root_name"
+PRODUCT_MAIN_CATEGORY: str = "subj_name"
+PRODUCT_DESCRIPTION: str = "description"
 
-PRODUCT_SELLING = "selling"
-PRODUCT_BRAND_NAME = "brand_name"
-PRODUCT_BRAND_ID_KEY = "brand_Id"
-PRODUCT_DETAIL_LIST_KEY = "options"
+PRODUCT_SELLING: str = "selling"
+PRODUCT_BRAND_NAME: str = "brand_name"
+PRODUCT_DETAIL_LIST_KEY: str = "options"
 
-PRODUCT_DETAIL_KEY_VALUES = "values"
-PRODUCT_DETAIL_KEY_VALUE = "value"
-PRODUCT_SIZE_DETAILS = "details"
+PRODUCT_DETAIL_KEY_VALUES: str = "values"
+PRODUCT_DETAIL_KEY_VALUE: str = "value"
+PRODUCT_SIZE_DETAILS: str = "details"
 
-PRODUCT_COMPOSITIONS = "compositions"
-PRODUCT_DETAIL_COMPOSITIONS = ["Состав", "Состав ювелирного изделия"]
+PRODUCT_COMPOSITIONS: str = "compositions"
+PRODUCT_DETAIL_COMPOSITIONS: list[str] = ["Состав", "Состав ювелирного изделия"]
 
-PRODUCT_SIZES_TABLE = "sizes_table"
-PRODUCT_SIZES_TABLE_TECH_SIZE_KEY = "tech_size"
-
+PRODUCT_SIZES_TABLE: str = "sizes_table"
 PRODUCT_MIN_SIZE = "min_size"
-
 PRODUCT_MAX_SIZE = "max_size"
 
-PRODUCT_DETAIL_COLOR = "Цвет"
-PRODUCT_COLOR = "color"
+PRODUCT_DETAIL_COLOR: str = "Цвет"
+PRODUCT_COLOR: str = "color"
 
-PRODUCT_DETAIL_MADE_IN = "Страна производства"
-PRODUCT_MADE_IN = "made_in"
+PRODUCT_DETAIL_MADE_IN: str = "Страна производства"
+PRODUCT_MADE_IN: str = "made_in"
 
-PRODUCT_PERSONAL_INFO_KEYS = [PRODUCT_DESCRIPTION,
-                              PRODUCT_CATEGORY,
-                              PRODUCT_MAIN_CATEGORY,
-                              PRODUCT_ROOT_ID,
-                              PRODUCT_NAME_KEY
-                              ]
+PRODUCT_PERSONAL_INFO_KEYS: list[str] = [PRODUCT_DESCRIPTION,
+                                         PRODUCT_CATEGORY,
+                                         PRODUCT_MAIN_CATEGORY,
+                                         PRODUCT_ROOT_ID,
+                                         PRODUCT_NAME_KEY
+                                         ]
 
-FEEDBACKS_KEY = "feedbacks"
-FEEDBACK_DATE_KEY = "createdDate"
-FEEDBACK_PRODUCT_ID_KEY = "nmId"
-FEEDBACK_COMMENT_KEY = "text"
-FEEDBACK_GRADE_KEY = "productValuation"
+FEEDBACKS_KEY: str = "feedbacks"
+FEEDBACK_DATE_KEY: str = "createdDate"
+FEEDBACK_PRODUCT_ID_KEY: str = "nmId"
+FEEDBACK_COMMENT_KEY: str = "text"
+FEEDBACK_GRADE_KEY: str = "productValuation"
 
-FEEDBACK_LAST_INDEX_OF_DATE_STR = 10
+FEEDBACK_LAST_INDEX_OF_DATE_STR: int = 10
 
-FEEDBACK_COMMENT_TITLE = 'comment'
-FEEDBACK_GRADE_TITLE = 'grade'
+FEEDBACK_COMMENT_TITLE: str = 'comment'
+FEEDBACK_GRADE_TITLE: str = 'grade'
 
-PRODUCT_CARD_JSON_TITLE = "card_json_url"
-PRODUCT_PRICE_HISTORY_JSON_TITLE = "price_history_json_url"
+PRODUCT_CARD_JSON_TITLE: str = "card_json_url"
+PRODUCT_PRICE_HISTORY_JSON_TITLE: str = "price_history_json_url"
 
-PRODUCT_LIST_URL = f"https://recom.wb.ru/personal/ru/common/v5/search?ab_testing=false&curr=rub&dest=-1257786&page={SYMBOL_TO_REPLACE_FOR_PAGE_NUMBER_IN_URL}&query=0&resultset=catalog"
+PRODUCT_LIST_URL: str = f"https://recom.wb.ru/personal/ru/common/v5/search?ab_testing=false&curr=rub&dest=-1257786&page={SYMBOL_TO_REPLACE_FOR_PAGE_NUMBER_IN_URL}&query=0&resultset=catalog"
 
-PRODUCT_URL = f"https://www.wildberries.ru/catalog/{PRODUCT_ID}/detail.aspx"
+PRODUCT_URL: str = f"https://www.wildberries.ru/catalog/{PRODUCT_ID}/detail.aspx"
 
-FEEDBACK_URLS = [f"https://feedbacks1.wb.ru/feedbacks/v2/{ROOT_ID}",
-                 f"https://feedbacks2.wb.ru/feedbacks/v2/{ROOT_ID}"]
+FEEDBACK_URLS: list[str] = [f"https://feedbacks1.wb.ru/feedbacks/v2/{ROOT_ID}",
+                            f"https://feedbacks2.wb.ru/feedbacks/v2/{ROOT_ID}"]
 
-THREADS_COUNT = 10
+# Count of threads in file 'primary_data_script.py'
+THREADS_COUNT: int = 10
 
 # Path to files that contain information about products
-PATH_TO_URLS = 'csv/urls.csv'
-PATH_TO_PRODUCTS = 'csv/products.csv'
-PATH_TO_PRICE_HISTORY = 'csv/price-history.csv'
-PATH_TO_FEEDBACK = 'csv/feedbacks.csv'
+PATH_TO_URLS: str = 'csv/urls.csv'
+PATH_TO_PRODUCTS: str = 'csv/products.csv'
+PATH_TO_PRICE_HISTORY: str = 'csv/price-history.csv'
+PATH_TO_FEEDBACK: str = 'csv/feedbacks.csv'
 
-INVALID_RESPONSE_MESSAGE = "Validator can't find this key in response: "
+INVALID_RESPONSE_MESSAGE: str = "Validator can't find this key in response: "
